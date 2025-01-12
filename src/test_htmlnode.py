0db00ad996b908ceb50cba8cfc05487611e26e53
oudotje <jean.oudot.06@hotmail.com>
2025-01-12 21:55:02 +0700

    Adding tests for the ParentNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_empty_init(self):
@@ -50,4 +50,40 @@ class TestLeafNode(unittest.TestCase):
     def test_to_html(self):
         leaf_node = LeafNode("a", "This is a test", {"href":"https://www.google.com"})
         self.assertEqual(leaf_node.to_html(), "<a href=\"https://www.google.com\">This is a test</a>")
-     
+    
+class TestParentNode(unittest.TestCase):
+    def test_init(self):
+        parent_node = ParentNode("p", [])
+        self.assertEqual(parent_node.tag, "p")
+        self.assertEqual(parent_node.value, None)
+        self.assertEqual(parent_node.children, [])
+        self.assertEqual(parent_node.props, None)
+
+    def test_to_html_emptychildren(self):
+        parent_node = ParentNode("p", [])
+        self.assertEqual(parent_node.to_html(), "<p></p>")
+
+
+    def test_to_html_leafchildren(self):
+        child1 = LeafNode("a","Click!", {"href":"https://www.google.com"})
+        child2 = LeafNode(None, "This is normal text")
+        child3 = LeafNode("i", "This is italic")
+        parent_node = ParentNode("p", [child1, child2, child3], {"test":"test_props"})
+
+        self.assertEqual(parent_node.to_html(), "<p test=\"test_props\"><a href=\"https://www.google.com\">Click!</a>This is normal text<i>This is italic</i></p>")
+
+    def test_to_html_parentchildren(self):
+        child1 = LeafNode("a", "Click!", {"href":"https://www.google.com"})
+        parent1 = ParentNode("p", [child1])
+        child2 = LeafNode("i", "This is italic")
+        parent2 = ParentNode("p", [parent1, child2], {"test":"test"})
+
+        self.assertEqual(parent2.to_html(), "<p test=\"test\"><p><a href=\"https://www.google.com\">Click!</a></p><i>This is italic</i></p>")
+
+    def test_to_html_nonetag(self):
+        parent_node = ParentNode(None, [])
+        self.assertRaises(ValueError, parent_node.to_html)
+
+    def test_to_html_nonechildren(self):
+        parent_node = ParentNode("p", None)
+        self.assertRaises(ValueError, parent_node.to_html)
