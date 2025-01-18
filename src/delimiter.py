f7d8e27e6274c8e17f5949388c4b15468e54a405
oudotje <jean.oudot.06@hotmail.com>
2025-01-18 17:54:37 +0700

    Added markdown extraction

--- src/delimiter.py
+++ src/delimiter.py
@@ -20,3 +20,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     new_nodes.append(TextNode(splitted_text[i], text_type))
     return new_nodes
 
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
